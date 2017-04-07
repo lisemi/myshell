749112a68b0349a8b4443a6b8f22ca956703d3f5
sammy <lisemi@163.com>
2017-04-07 16:43:14 +0800

    增加nfs等工具安装，优化代码，加入可选配置选项，可以单独选择配置某个工具

--- install_toolkit.sh
+++ install_toolkit.sh
@@ -10,15 +10,17 @@ USER_NAME=$USER
 GIT_NAME=sammy
 GIT_EMAIL=lisemi@163.com
 GIT_KEY=$USER_PATH/.ssh/id_rsa.pub
+NFS_PATH=$USER_PATH/nfs
 
+echo -e "\e[0;33;1m     exit| install | config\e[0m"
 echo -e "\e[0;33;1m     select toolkit name, default compile all\e[0m"
 echo -e "\e[0;33;1m     toolkit name list - \e[0m"
-echo -e "\e[0;33;1m       all | configure\e[0m"
 echo -e "\e[0;33;1m       g++ | flex | gettext   | makeinfo  | automake  | build-essential\e[0m"
 echo -e "\e[0;33;1m       git | lzop | help2man  | odblatex  | autoconf  | python3.5-dev\e[0m"
 echo -e "\e[0;33;1m       m4  | zsh  | indent    | docbook2x | mtd-utils | openssh-server\e[0m"
 echo -e "\e[0;33;1m       bc  | vim  | autopoint | bison     | md5sum    | indicator-netspeed\e[0m"
-echo -e "\e[0;33;1m       sambacommon| smbclient | samba     | cmake     | compizconfig-settings-manager\e[0m"
+echo -e "\e[0;33;1m       nfs | scons| samba     | cmake     | compizconfig-settings-manager\e[0m"
+echo -e "\e[0;33;1m       dbus|      | d-feet    | libgtk2.0 | libncurses5-dev\e[0m"
 echo -e "\e[0;32;1m   choose:\e[0m \c"
 read compile_args
 
@@ -48,16 +50,21 @@ package_name=(
 [22]=openssh-server
 [23]=indicator-netspeed             #拼音输入法
 [24]=compizconfig-settings-manager  #显示桌面:win + D
-[25]=samba
-[26]=samba-common
-[27]=smbclient
-[28]=zsh
-[29]=vim
+[25]="samba samba-common smbclient"
+[26]=zsh
+[27]=vim
+[28]=dbus                           #进程间通信
+[29]=d-feet                         #dbus信息查看工具: d-feet
+[30]=libgtk2.0-dev
+[31]=libncurses5-dev
+[32]=scons
+[33]="nfs-kernel-server nfs-common"
 )
 
 trap - INT
 
 case $compile_args in
+	install)                    toolkit_index=0;;
 	g++)                        toolkit_index=1;;
 	build-essential+)           toolkit_index=2;;
 	git)						toolkit_index=3;;
@@ -83,12 +90,17 @@ case $compile_args in
 	indicator-netspeed)			toolkit_index=23;;
 	compizconfig-settings-manager) toolkit_index=24;;
 	samba)						toolkit_index=25;;
-	sambacommon)				toolkit_index=26;;
-	smbclient)					toolkit_index=27;;
-	zsh)						toolkit_index=28;;
-	vim)						toolkit_index=29;;
-	configure)					toolkit_index=100;;
-	*)                          toolkit_index=0;;
+	zsh)						toolkit_index=26;;
+	vim)						toolkit_index=27;;
+	dbus)						toolkit_index=28;;
+	d-feet)						toolkit_index=29;;
+	libgtk2.0)					toolkit_index=30;;
+	libncurses5-dev)            toolkit_index=31;;
+	scons)                      toolkit_index=32;;
+	nfs)                        toolkit_index=33;;
+	config)					    toolkit_index=1000;;
+	exit)                       exit 0;;
+	*)                          echo -e "\e[0;32;1m[info] : invalid arguments\e[0m"; exit 0;;
 esac
 
 
@@ -104,7 +116,7 @@ function install_all(){
 if [ $toolkit_index = "0" ]; then
 	echo -e "\e[0;32;1m[info] : install all toolkit\e[0m"
     install_all
-elif [ $toolkit_index -gt 0 -a $toolkit_index -lt 30 ];then
+elif [ $toolkit_index -gt 0 -a $toolkit_index -lt 100 ];then
 	tool=${package_name[$toolkit_index]}
 	echo -e "\e[0;32;1m[info] : install $tool\e[0m"
 	sudo apt-get -y --assume-yes install $tool
@@ -113,52 +125,118 @@ fi
 
 # configure ========================================================
 # 不是安装全部或者指定配置，则直接退出，不进行配置
-if [ $toolkit_index -eq 0 -o $toolkit_index -eq 100 ];then
-	echo -e "\033[0;32;1m[info] : support configure"
+# if [ $toolkit_index -eq 0 -o $toolkit_index -eq 100 ];then
+	# echo -e "\033[0;32;1m[info] : support configure"
+# else
+	# exit 0
+# fi
+
+function config_zsh(){
+	echo -e "\033[0;32;1m[info] : detection zsh\e[0m"
+	if [ ! -f $HOME/.zshrc ]; then
+		echo -e "\033[0;32;1m[info] : configure zsh\e[0m"
+		wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+		sudo usermod -s /bin/zsh $USER
+	fi
+}
+
+function config_samba(){
+	echo -e "\033[0;32;1m[info] : detaction samba\e[0m"
+	grep "$USER_PATH" /etc/samba/smb.conf > /dev/null 2>&1
+	if [ $? != "0" ];then
+		echo -e "\033[0;32;1m[info] : configure samba\e[0m"
+		sudo sed -i '44a hosts allow = 192.168.199.138' /etc/samba/smb.conf
+		sudo sed -i '$a ['$USER_NAME']' /etc/samba/smb.conf
+		sudo sed -i '$a path='$USER_PATH'' /etc/samba/smb.conf
+		sudo sed -i '$a available = yes' /etc/samba/smb.conf
+		sudo sed -i '$a browseable = yes' /etc/samba/smb.conf
+		sudo sed -i '$a public = yes' /etc/samba/smb.conf
+		sudo sed -i '$a writable = yes' /etc/samba/smb.conf
+		sudo sed -i '$a create mask = 0775' /etc/samba/smb.conf
+		sudo sed -i '$a directory mask = 0775' /etc/samba/smb.conf
+	fi
+}
+
+function config_ssh(){
+	echo -e "\033[0;32;1m[info] : detaction ssh\e[0m"
+	ps -x | grep "sshd" | grep -v grep > /dev/null 2>&1
+	if [ $? != 0 ]; then
+		echo -e "\033[0;32;1m[info] : configure ssh\e[0m"
+		/etc/init.d/ssh start
+	fi
+}
+
+function config_git(){
+	echo -e "\033[0;32;1m[info] : detaction git\e[0m"
+	if [ ! -f $GIT_KEY ]; then
+		echo -e "\033[0;32;1m[info] : configure git"
+		ssh-keygen -t rsa -C "$GIT_EMAIL"
+		git config --global user.name "$GIT_NAME"
+		git config --global user.email "$GIT_EMAIL"
+	fi
+}
+
+function config_vim(){
+	echo -e "\033[0;32;1m[info] : configure vim\e[0m"
+	if [ ! -d $USER_PATH/.vim/bundle/vundle ]; then
+		git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+		git@github.com:lisemi/vimrc.git $USER_PATH
+		cp $USER_PATH/vimrc/vimrc ~/.vimrc
+		rm -r $USER_PATH/vimrc
+	fi
+}
+
+function config_nfs(){
+	echo -e "\033[0;32;1m[info] : configure nfs\e[0m"
+	if [ -f /etc/exports ]; then
+		grep "$NFS_PATH" /etc/exports > /dev/null 2>&1
+		if [ $? != "0" ]; then
+			sudo sed -i '$a '$NFS_PATH' *(rw,sync,no_root_squash)' /etc/exports
+			# “ *” 表示允许任何网段 IP 的系统访问该 NFS 目录
+			sudo /etc/init.d/nfs-kernel-server start
+			if [ $? != "0" ]; then
+				echo -e "\033[0;32;1m[info] : nfs start fail; now restart!\e[0m"
+				sudo /etc/init.d/nfs-kernel-server restart
+				if [ $? != 0 ]; then
+					echo -e "\033[0;32;1m[info] : nfs restart fail!\e[0m"
+				fi
+			else
+				echo -e "\033[0;32;1m[info] : nfs start ok!\e[0m"
+			fi
+		else
+			echo -e "\033[0;32;1m[info] : The nfs directory has been configured!\e[0m"
+		fi
+	fi
+}
+
+function config_all(){
+	config_zsh
+	config_samba
+	config_ssh
+	config_git
+	config_vim
+	config_nfs
+}
+
+
+if [ $toolkit_index = "0" -o $toolkit_index = "1000" ]; then
+	echo -e "\e[0;33;1m     select toolkit name, default configure all\e[0m"
+	echo -e "\e[0;33;1m     toolkit name list - \e[0m"
+	echo -e "\e[0;33;1m       all | zsh | samba | ssh  | git  | vim\e[0m"
+	echo -e "\e[0;33;1m       nfs | \e[0m"
+	echo -e "\e[0;32;1m   choose:\e[0m \c"
+	read config_args
 else
 	exit 0
 fi
 
-echo -e "\033[0;32;1m[info] : detection zsh\e[0m"
-if [ ! -f $HOME/.zshrc ]; then
-	echo -e "\033[0;32;1m[info] : configure zsh\e[0m"
-	wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
-	sudo usermod -s /bin/zsh $USER
-fi
-
-echo -e "\033[0;32;1m[info] : detaction samba"
-grep "$USER_PATH" /etc/samba/smb.conf > /dev/null 2>&1
-if [ $? != "0" ];then
-	echo -e "\033[0;32;1m[info] : configure samba"
-	sudo sed -i '$a ['$USER_NAME']' /etc/samba/smb.conf
-	sudo sed -i '$a path='$USER_PATH'' /etc/samba/smb.conf
-	sudo sed -i '$a available = yes' /etc/samba/smb.conf
-	sudo sed -i '$a browseable = yes' /etc/samba/smb.conf
-	sudo sed -i '$a public = yes' /etc/samba/smb.conf
-	sudo sed -i '$a writable = yes' /etc/samba/smb.conf
-	sudo sed -i '$a create mask = 0775' /etc/samba/smb.conf
-	sudo sed -i '$a directory mask = 0775' /etc/samba/smb.conf
-fi
-
-echo -e "\033[0;32;1m[info] : detaction ssh"
-ps -x | grep "sshd" | grep -v grep > /dev/null 2>&1
-if [ $? != 0 ]; then
-	echo -e "\033[0;32;1m[info] : configure ssh"
-	/etc/init.d/ssh start
-fi
-
-echo -e "\033[0;32;1m[info] : detaction git"
-if [ ! -f $GIT_KEY ]; then
-	echo -e "\033[0;32;1m[info] : configure git"
-	ssh-keygen -t rsa -C "$GIT_EMAIL"
-	git config --global user.name "$GIT_NAME"
-	git config --global user.email "$GIT_EMAIL"
-fi
-
-echo -e "\033[0;32;1m[info] : configure vim"
-if [ ! -d $USER_PATH/.vim/bundle/vundle ]; then
-	git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-	git@github.com:lisemi/vimrc.git $USER_PATH
-	cp $USER_PATH/vimrc/vimrc ~/.vimrc
-	rm -r $USER_PATH/vimrc
-fi
+case $config_args in
+	all)      config_all;;
+	zsh)      config_zsh;;
+	samba)    config_samba;;
+	ssh)      config_ssh;;
+	git)      config_git;;
+	vim)      config_vim;;
+	nfs)      config_nfs;;
+	*)        echo -e "\e[0;32;1m[info] : invalid arguments\e[0m"
+esac
